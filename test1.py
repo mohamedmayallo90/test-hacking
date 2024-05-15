fe274bc66f0ac4d214c072e1c8390c2f29ec5d57
mohamedmayallo90 <mohamedmayallo90@gmail.com>
2024-05-15 09:34:31 +0300

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,50 @@
+import os
+os.system("clear")
+print(' ')
+print('\033[1;31m                                                                        _,.-----.,_')
+print('\033[1;31m                                                                      ,-~           ~-.')
+print('\033[1;31m                                                                    ,^___           ___^.')
+print('\033[1;31m                                                                   /~"   ~"   .   "~   "~')
+print('\033[1;31m                                                                  Y  ,--._    I    _.--.  Y')
+print('\033[1;31m                                                                  | Y     ~-. | ,-~     Y |')
+print('\033[1;31m                                                                  | |        }:{        | |')
+print('\033[1;31m                                                                  j l       / | \       ! l')
+print('\033[1;31m                                                               .-~  (__,.--" .^. "--.,__)  ~-.')
+print('\033[1;31m                                                             (           / / | \ \           )')
+print('\033[1;31m                                                               \.____,   ~  \/"\/  ~   .____,/')
+print('\033[1;31m                                                                ^.____                 ____.^')
+print('\033[1;32m                                                                   | |T ~\  !   !  /~ T| |')
+print('\033[1;32m                                                                   | |l   _ _ _ _ _   !| |')
+print('\033[1;32m                                                                   | l \/V V V V V V\/ j |')
+print('\033[1;32m                                                                   l  \ \|_|_|_|_|_|/ /  !')
+print('\033[1;32m                                                                    \  \[T T T T T TI/  /')
+print('\033[1;32m                                                                     \  `^-^-^-^-^-^'  /'')
+print('\033[1;32m                                                                      \               /  \033[1;34m-ElMOZ3G')
+print('\033[1;32m                                                                       \.           ,/')
+print('\033[1;32m                                                                        "^-.___,-^"')
+name =input("\033[1;31m text your name-->>")
+print("weclome"+name)
+print("1-get locatin")
+print("2-hacker android")
+print("3-hacker windows")
+print("4-kali linux")
+print("5-Exit")
+mo =input("\033[1;31m ElMOZ3G-->>")
+if mo=="1":
+    print("ok thanks1")
+    mo =input("\033[1;31m ElMOZ3G-->>")
+if mo=="2":
+    print("ok thanks2")
+    mo =input("\033[1;31m ElMOZ3G-->>")
+    if mo=="3":
+        print("ok thanks3")
+        if mo=="4":
+            print("ok thanks4")
+            if mo=="5":
+                os.system("clear")
+                os.system("exit")
+    
+else:
+    print("\033[1;34m Please write something")
+    mo =input("\033[1;31m ElMOZ3G-->>")
+   
